5b7b0dd573c9b3a7153c94d94f46e0efd87707c1
VitaliaOrmanzhy <vormanzi812@gmail.com>
2023-06-27 15:22:32 +0300

    converted strings to numbers

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const a = prompt('First num');
-const b = prompt('Second num');
+const a = +prompt('First num');
+const b = +prompt('Second num');
 alert(`${a}+${b}=${a+b}
 ${a}-${b}=${a-b}
 ${a}*${b}=${a*b}
